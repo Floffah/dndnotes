94accf9172daaac21936707c3fb41b2e54555c66
Floffah <floffah@floffah.dev>
2023-12-13 17:41:17 +0000

    campaign back button

--- src/app/campaign/[campaignId]/CampaignNavBar.tsx
+++ src/app/campaign/[campaignId]/CampaignNavBar.tsx
@@ -12,6 +12,19 @@ export function CampaignNavBar() {
     return (
         <div className="flex items-center gap-2 rounded-lg border border-white/10 bg-white/5 p-2">
             <Link
+                href="/"
+                className="flex cursor-pointer select-none items-center justify-center p-1 transition-all duration-150 hover:scale-110"
+            >
+                <Icon
+                    label="leave campaign icon"
+                    icon="mi:arrow-left"
+                    className="h-5 w-5"
+                />
+            </Link>
+
+            <Divider orientation="vertical" />
+
+            <Link
                 href={`/campaign/${campaign.id}`}
                 className="text-lg font-semibold"
             >

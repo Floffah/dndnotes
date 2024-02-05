ff6a14f0eca6789af6c88835521786933fdac06e
Floffah <floffah@floffah.dev>
2024-02-05 19:15:16 +0000

    fix: build fixes

--- src/db/models/User/consumers.ts
+++ src/db/models/User/consumers.ts
@@ -2,16 +2,15 @@ import type { User } from "@/db/models/User/index";
 import { BaseAPIModel } from "@/db/types/baseModel";
 import { ConsumerContext } from "@/db/types/consumerContext";
 
-export class UserAPIModel
-    extends BaseAPIModel
-    implements Omit<User, "providers">
-{
+export class UserAPIModel extends BaseAPIModel implements User {
     email: string;
     name: string;
+    providers: User["providers"];
 
     constructor(user: User, ctx: ConsumerContext) {
         super(user, ctx);
         this.name = user.name;
         this.email = ctx.user && user.id === ctx.user.id ? user.email : null!;
+        this.providers = null!;
     }
 }

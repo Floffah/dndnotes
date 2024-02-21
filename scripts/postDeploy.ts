43e66cac84032ed12ae082424d64f6e7d68bb3c4
Floffah <floffah@floffah.dev>
2024-02-21 12:29:03 +0000

    🔨 ci: update post deploy script to support gha

--- scripts/postDeploy.ts
+++ scripts/postDeploy.ts
@@ -1,32 +1,53 @@
+import * as fs from "fs";
 import { EmbedBuilder, WebhookClient } from "discord.js";
 import "dotenv/config";
+import { readCommit } from "isomorphic-git";
 
-const client = new WebhookClient({
-    url: process.env.DISCORD_WEBHOOK_LOGS as string,
-});
+async function run() {
+    const client = new WebhookClient({
+        url: process.env.DISCORD_WEBHOOK_LOGS as string,
+    });
 
-client.send({
-    embeds: [
-        new EmbedBuilder()
-            .setTitle("Deployment finished")
-            .setDescription(
-                "Build finished - wait for domains and functions to update.",
-            )
-            .setURL(
-                `https://github.com/floffah/dndnotes/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`,
-            )
-            .addFields(
-                {
-                    name: "commit",
-                    value: `(${process.env.VERCEL_GIT_COMMIT_SHA}) ${process.env.VERCEL_GIT_COMMIT_MESSAGE}`,
-                    inline: true,
-                },
-                {
-                    name: "author",
-                    value:
-                        process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME || "unknown",
-                    inline: true,
-                },
-            ),
-    ],
-});
+    let commitSha = process.env.VERCEL_GIT_COMMIT_SHA || process.env.GITHUB_SHA;
+    let commitMessage = process.env.VERCEL_GIT_COMMIT_MESSAGE;
+
+    if (!commitMessage && commitSha) {
+        const result = await readCommit({
+            fs,
+            dir: process.cwd(),
+            oid: commitSha,
+        });
+
+        commitMessage = result.commit.message;
+    }
+
+    await client.send({
+        embeds: [
+            new EmbedBuilder()
+                .setTitle("Deployment finished")
+                .setDescription(
+                    "Build finished - wait for domains and functions to update.",
+                )
+                .setURL(
+                    `https://github.com/floffah/dndnotes/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`,
+                )
+                .addFields(
+                    {
+                        name: "commit",
+                        value: `(${commitSha || "no commit sha"}) ${commitMessage || "no message"}`,
+                        inline: true,
+                    },
+                    {
+                        name: "author",
+                        value:
+                            process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME ||
+                            process.env.GITHUB_ACTOR ||
+                            "unknown",
+                        inline: true,
+                    },
+                ),
+        ],
+    });
+}
+
+run();

5e4b9d7d34e9f563313efcd7d87bad884319aa8f
Floffah <floffah@floffah.dev>
2024-06-06 13:51:36 +0000

    fix: return existing mongoose model instead of overwriting in production

--- apps/api/src/lib/createModel.ts
+++ apps/api/src/lib/createModel.ts
@@ -1,4 +1,28 @@
-import { Schema, model } from "mongoose";
+import {
+    HydratedDocument,
+    InferSchemaType,
+    Model,
+    ObtainSchemaGeneric,
+    Schema,
+    model,
+    models,
+} from "mongoose";
+
+type MongooseCreateModelReturn<TSchema extends Schema> = Model<
+    InferSchemaType<TSchema>,
+    ObtainSchemaGeneric<TSchema, "TQueryHelpers">,
+    ObtainSchemaGeneric<TSchema, "TInstanceMethods">,
+    ObtainSchemaGeneric<TSchema, "TVirtuals">,
+    HydratedDocument<
+        InferSchemaType<TSchema>,
+        ObtainSchemaGeneric<TSchema, "TVirtuals"> &
+            ObtainSchemaGeneric<TSchema, "TInstanceMethods">,
+        ObtainSchemaGeneric<TSchema, "TQueryHelpers">
+    >,
+    TSchema
+> &
+    ObtainSchemaGeneric<TSchema, "TStaticMethods">;
+// type MongooseCreateModelReturn<TSchema extends Schema> = ReturnType<typeof model<TSchema>>;
 
 export function createModel<S extends Schema>(name: string, schema: S) {
     schema.virtual("id").get(function (this: any) {
@@ -22,6 +46,10 @@ export function createModel<S extends Schema>(name: string, schema: S) {
             delete ret.__v;
         },
     });
+
+    if (process.env.NODE_ENV === "production" && name in models) {
+        return models[name] as MongooseCreateModelReturn<S>;
+    }
 
     return model(name, schema, undefined, {
         overwriteModels: process.env.NODE_ENV === "development",

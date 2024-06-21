1b9ed4910a55caefea15ca2782fd8d5610f9ccba
therealfloffah@gmail.com <therealfloffah@gmail.com>
2024-06-21 14:32:29 +0100

    feat: site logo

--- apps/web/src/lib/populateMetadata.ts
+++ apps/web/src/lib/populateMetadata.ts
@@ -1,14 +1,16 @@
 import { Metadata } from "next";
+import type { Icon } from "next/dist/lib/metadata/types/metadata-types";
 
 interface PopulateMetadataOptions {
     title?: string;
     description?: string;
     image?: string;
+    icons?: Icon[];
 }
 
 export function populateMetadata(
     { title, description, image }: PopulateMetadataOptions,
-    override: Metadata = {},
+    override: Omit<Metadata, "icons"> = {},
 ): Metadata {
     return {
         metadataBase: new URL("https://dndnotes.app"),
@@ -24,6 +26,32 @@ export function populateMetadata(
             description,
             ...override.twitter,
         },
+        icons: [
+            {
+                url: "/logos/dndnotes16.png",
+                rel: "icon",
+                type: "image/png",
+                sizes: "16x16",
+            },
+            {
+                url: "/logos/dndnotes32.png",
+                rel: "icon",
+                type: "image/png",
+                sizes: "32x32",
+            },
+            {
+                url: "/logos/dndnotes64.png",
+                rel: "icon",
+                type: "image/png",
+                sizes: "64x64",
+            },
+            {
+                url: "/logos/dndnotes.png",
+                rel: "icon",
+                type: "image/png",
+                sizes: "1024x1024",
+            },
+        ],
         ...override,
     };
 }
